d12171b9ecf569e5f6024d3d227cfa4df510ccee
Viet Hoang <hoangviet18306@gmail.com>
2021-06-02 15:13:30 +0700

    commit second unknow

--- BaiThiHSGTpHCM202101/BaiThiHSGTpHCM202101/CayVaNhungNguoiBan.cpp
+++ BaiThiHSGTpHCM202101/BaiThiHSGTpHCM202101/CayVaNhungNguoiBan.cpp
@@ -6,3 +6,71 @@
 //
 
 #include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <cmath>
+class Corrdinate {
+private:
+    int x;
+    int y;
+public:
+    Corrdinate(){};
+    Corrdinate(int xValue, int yValue):x{xValue},y{yValue}{std::cout<<"Created corrdinate"<<std::endl;};
+    int getX() {return x;};
+    int getY() {return y;};
+    ~Corrdinate(){std::cout<<"An corrdinate has been gone!!!"<<std::endl;};
+};
+class Tree {
+private:
+    Corrdinate corrdinate;
+    int height;
+    int group;
+public:
+    Tree(){};
+    Tree(Corrdinate corr,int heightValue);
+    ~Tree(){std::cout<<"destroyed object"<<std::endl;};
+    int getHeight(){return height;};
+    Corrdinate getcorr (){return corrdinate;};
+    void setHeight(int);
+};
+Tree::Tree(Corrdinate corr,int heightValue){
+    corrdinate = corr;
+    if((corr.getX()/2)>=(corr.getY()/2)){
+        group = corr.getX()/2;
+    }else{
+        group = corr.getY()/2;
+    }
+}
+void Tree::setHeight(int heightValue){
+    height = heightValue;
+}
+int main(){
+    std::ifstream Inp;
+    std::ofstream Oup;
+    std::cout<<1/2<<std::endl;
+    Inp.open("CHEVUON.INP");
+    Oup.open("CHEVUON.OUT");
+    if(Inp){
+        int x;
+        int y;
+        std::set<int> heightValue;
+        std::vector<Tree> jungle {};
+        Inp>>x;
+        Inp>>y;
+        for (int i{0}; i<x; ++i) {
+            for (int j{0}; i<y; ++j) {
+                int temp;
+                Inp>>temp;
+                heightValue.insert(temp);
+                jungle.push_back(Tree(Corrdinate(i, j), 0));
+            }
+        }
+        
+    }
+    Inp.close();
+    Oup.close();
+    return 0;
+}
--- BaiThiHSGTpHCM202101/BaiThiHSGTpHCM202101/Count.cpp
+++ BaiThiHSGTpHCM202101/BaiThiHSGTpHCM202101/Count.cpp
@@ -6,3 +6,99 @@
 //
 
 #include <stdio.h>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+std::vector<std::string> pharse (std::string);
+std::vector<std::string> calculate (std::vector<std::string>);
+int repeatedLetter (std::string);
+int repetedWord (std::string, std::vector<std::string>);
+bool checkexist (std::vector<std::string>,std::string);
+int main02(){
+    std::ifstream Inp;
+    std::ofstream Oup;
+    Inp.open("DEMTU.INP");
+    Oup.open("DEMTU.OUT");
+    if(Inp){
+        std::vector<std::string> vectorString {};
+        std::vector<std::string> result {};
+        while(!Inp.eof()){
+            std::string string;
+            getline(Inp,string);
+            vectorString.push_back(string);
+        }
+        std::cout<<"end of file" <<std::endl;
+        for(auto string:vectorString){
+            std::vector<std::string> tempresult = calculate(pharse(string));
+            for (auto elem:tempresult){
+                result.push_back(elem);
+            }
+        }
+        Oup<<result.size()<<std::endl;
+        for (auto elem:result){
+            Oup<<elem<<" ";
+        }
+        std::cout<<"end of program"<<std::endl;
+    }
+    Inp.close();
+    Oup.close();
+    return 0;
+}
+std::vector<std::string> pharse (std::string base){
+    std::vector<std::string> result {};
+    std::stringstream ss {base};
+    std::string word {};
+    while (ss>>word) {
+        result.push_back(word);
+    }
+    return result;
+}
+std::vector<std::string> calculate (std::vector<std::string> word){
+    std::vector<std::string> result {};
+    if (word.size() != 0){
+        for (auto elem:word){
+            int repeatedwordTime = repetedWord(elem, word);
+            if (repeatedwordTime > 1){
+                result.push_back(elem);
+            }
+        }
+        for(auto elem:word){
+            int repeatedTime = repeatedLetter(elem);
+            if (repeatedTime>elem.length()) {
+                result.push_back(elem);
+            }
+        }
+    }
+    return result;
+}
+bool checkexist (std::vector<std::string> vector,std::string defination){
+    for(auto elem:vector ){
+        if (defination == elem) {
+            return false;
+        }
+    }
+    return true;
+}
+int repeatedLetter (std::string pharse){
+    std::string tempPharse {pharse};
+    int result {0};
+    for(auto elem:tempPharse){
+        for (auto elems:tempPharse) {
+            if (elem==elems) {
+                ++result;
+            }
+        }
+    }
+    return result;
+}
+int repetedWord (std::string word, std::vector<std::string> vec){
+    int result {0};
+    for (auto elem:vec){
+        if (word == elem){
+            ++result;
+        }
+    }
+    return result;
+}
